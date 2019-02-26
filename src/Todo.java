f19181f9045bfac8889b3a123cb2fa9711c6eea1
Aaron Craig <aaron.t.craig-1@ou.edu>
2019-02-26 13:07:09 -0600

    added a todo

---
+++ src/Todo.java
@@ -0,0 +1,42 @@
+public class Todo {/*
+Lab Section_________: 011
+Last Modified_______: 2019-02-26
+OU ID_______________: 113365209
+OU EMAIL____________: aaron.t.craig-1@ou.edu
+
+Tasks:
+1. Create and complete rectangle class
+2. Create and complete square class
+3. Finish polygon class
+4. Finish circle class, equilateralTriangle class, and Trapezoid class
+5. Finish ShapeAreaComparator class
+6. Complete ShapePerimeterComparator class
+7. Complete Shape class
+8. Complete all unit tests for ShapeTest
+9. Complete all unit tests for ShapeSorterTest
+10. Run and debug all classes
+
+Estimated Time (minutes):
+1. 5
+2. 5
+3. 5
+4. 30
+5. 5
+6. 5
+7. 10
+8. 20
+9. 20
+10. 30
+
+Actual Time (minutes):
+1. 5
+2. 5
+3. 10
+4. 45
+5. 10
+6. 10
+7. 20
+8. 30
+9. 45
+10. 30
+*/}
